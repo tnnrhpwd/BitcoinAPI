a0cd3299c1a354d19f9f062ed7d7d29a14641c73
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-03-31 10:53:24 -0500

    Rewrote the getPrice function
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- bitcoin.js
+++ bitcoin.js
@@ -1,8 +1,18 @@
-const getPrice = () => {
-    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
-    .then(data => data.json())
-    .then(data => console.log(data))
-};
+// const getPrice = () => {
+//     fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.log('ERROR'))
+// };
+
+const url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
+async function getPrice(){
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+}
+
 
 getPrice();
 console.log("hi")
