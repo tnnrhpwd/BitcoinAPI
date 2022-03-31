4bb52998bcf135cce877dc4d573d94740da59c4b
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-03-31 11:17:54 -0500

    cleaned up the code + added comments
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- bitcoin.js
+++ bitcoin.js
@@ -1,18 +1,19 @@
 const url = "https://api.coindesk.com/v1/bpi/currentprice.json";        // Initialize the API URL
-let priceSpace= document.getElementById("price");
+let priceSpace= document.getElementById("price");                       // Initialize the price HTML ID variable
 let price;                                                              // Initialize the price string variable 
 
-// returns string of bitcoin price
+// returns float of bitcoin price
 async function getPrice(){                                              // Async function(waits for data to execute) that returns the price of Bitcoin
     const response = await fetch(url);                                  // fetch json
     const data = await response.json();                                 // assign json to variable
     //price = (data.bpi.USD.rate);                                        // assign the price string to variable
-    publishPrice((data.bpi.USD.rate));
+    publishPrice((data.bpi.USD.rate_float.toFixed(2)));
 
+}
+
+// function that outputs its input to the html page
+function publishPrice(prc){
+    priceSpace.innerHTML = `<div>`+prc+`</div>`; // ID price section
 }
 
 getPrice();
-
-function publishPrice(prc){
-    priceSpace.innerHTML = `<div class="price">`+prc+`</div>`; // id price section
-}
